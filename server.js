8c07bb2ddd88dfae0eb4e3a9bce47bfd5fb60212
KagariSoft-Dev <dev@kagarisoft.com>
2023-01-27 18:48:22 -0300

    update fly.toml and server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors("*"));
-app.use('/repositories', express.static(path.join(__dirname, 'src', 'repositories')))
+app.use('/repositories', express.static(path.join(__dirname, 'data', 'repositories')))
 
 app.use("/api", endpoints)
 
