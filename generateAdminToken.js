ffb81b1cbef8c4094cff8a009290d1d904886fbf
KagariSoft-Dev <dev@kagarisoft.com>
2023-01-27 00:07:09 -0300

    update generateAdminToken.js

--- generateAdminToken.js
+++ generateAdminToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 
-const admin = false; // By default admin is true, if the admin token is not
+const admin = true; // By default admin is true, if the admin token is not
                     // specified in the request this app return 403
                     
                     // For extra security reasons it is recommended to modify the name of the variable or use it in another way, since if a token is created in this way and it is admin true anyone will be able to access the api with that token. For a basic use you can leave it as it is or change it for another name.
